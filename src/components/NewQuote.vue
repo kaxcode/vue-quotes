b1808d5c0ec324d58133b0e88bce18133bd9a392
Kenia <keniavalladarez@gmail.com>
2018-09-14 12:57:23 -0400

    Create NewQuote component.

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="row">
+    <form action="">
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form group">
+        <label>Quote</label>
+        <textarea class="form-control" rows="3" v-model="quote"></textarea>
+      </div>
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form group">
+        <button class="btn btn-primary" @click.prevent="createNew">
+          Add Quote
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      quote: ""
+    };
+  },
+  methods: {
+    createNew() {
+      this.$emit("quoteAdded", this.quote);
+      this.quote = "";
+    }
+  }
+};
+</script>
+
+<style scoped>
+.panel-body {
+  font-family: "Arizonia", cursive;
+  font-size: 24px;
+  color: #6e6e6e;
+}
+
+.quote {
+  cursor: pointer;
+}
+
+.quote:hover {
+  background-color: #ffe2e2;
+}
+</style>
+
