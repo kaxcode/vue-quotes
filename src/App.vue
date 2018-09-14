5472259c071b05fc6032b5ceb96d6743a7ef840e
Kenia <keniavalladarez@gmail.com>
2018-09-14 12:57:26 -0400

    Import NewQuote and QuoteGrid into file to view.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
     <div class="container">
-
+      <app-new-quote @quateAdded="newQuote"></app-new-quote>
+      <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
+import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
+
 export default {
   data: function() {
     return {
       quotes: ["Quote"],
       maxQuotes: 10
     };
+  },
+  methods: {
+    newQuote(quote) {
+      this.quotes.push(quote);
+    }
+  },
+  components: {
+    appNewQuote: NewQuote,
+    appQuoteGrid: QuoteGrid
   }
 };
 </script>
