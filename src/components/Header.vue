01e5f2f268b7ab08c420b5599b97e5706e9e1f35
Kenia <keniavalladarez@gmail.com>
2018-09-14 13:52:13 -0400

    Create Header component.

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="row">
+    <div class="col-sm-12">
+      <h3>Quotes Added</h3>
+      <div class="progress">
+        <div class="progress">
+          <div class="progress-bar" role="progressbar" area-valuenow="60" area-valuemin="0" area-valuemax="100" :style="{width: (quoteCount /maxQuotes) * 100 + '%'}">
+          {{ quoteCount}} / {{ maxQuotes}}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["quoteCount", "maxQuotes"]
+};
+</script>
